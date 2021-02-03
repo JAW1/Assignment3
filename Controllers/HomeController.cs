8d2cfb658413a115b4e5204548031a48c4590989
JAW1 <Jakewelling@gmail.com>
2021-02-02 19:56:41 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        //Added by Me
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddMovie(AddMovie addMovie)
+        {
+            TempStorage.AddTheMovie(addMovie);
+            
+            return View();
+        }
+        public IActionResult ListMovies()
+        {
+            return View(TempStorage.Movies);
+        }
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        //end added to me
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//this page is to store movies
+namespace Assignment3.Models
+{ 
+    public class TempStorage
+    {
+        private static List<AddMovie> movies = new List<AddMovie>();
+
+        public static IEnumerable<AddMovie> Movies => movies;
+
+        public static void AddTheMovie(AddMovie movie)
+        {
+            movies.Add(movie);
+        }
+    }
+}
